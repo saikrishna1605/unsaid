0801e661f88cca78a459cf9c5e6fd507aa5ac057
Saikrishna M <saikrishnamukka6699@gmail.com>
2026-02-07 12:29:02 +0000

    File changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "studio-7159486057-b2d89",
-  "appId": "1:826787982444:web:f5c8eafbe12313d4163c49",
   "apiKey": "AIzaSyDrznqGAx1l2UCdedvfKyZ0j7biwfmFaiw",
   "authDomain": "studio-7159486057-b2d89.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "826787982444"
+  "projectId": "studio-7159486057-b2d89",
+  "storageBucket": "studio-7159486057-b2d89.appspot.com",
+  "messagingSenderId": "826787982444",
+  "appId": "1:826787982444:web:f5c8eafbe12313d4163c49"
 };
